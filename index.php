bf463f37b073323a1906fa90bc80c6f2bfd68b52
Marcus <marcuschanjh@gmail.com>
2018-10-06 00:42:19 +1000

    Use Card Instead and test

--- index.php
+++ index.php
@@ -108,16 +108,18 @@
 
         <div class="card-columns">
 
-            <div class="card" style="width: 18rem;">
+
+
+            <div class="card" style="width 18rem;">
+                <div class="card-header">$row['title']</div>
                 <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="card-link">Card link</a>
-                    <a href="#" class="card-link">Another link</a>
+                    <p class="card-text">$row['file']</p>
                 </div>
+                <div class="card-footer"><a href="#" class="card-link">Edit</a>
+                    <a href="#" class="card-link">Delete</a></div>
             </div>
 
+            
 
         </div>
     </div>
